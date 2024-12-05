4bf1e2ecc2e8edf4755d57516a265516af31aa9e
Mark John Custer <mark_john_custer@dlsu.edu.ph>
2024-12-05 14:40:08 +0800

    All Machines Playable Music

---
+++ Oculus Project/Assets/Cassette.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+public class Cassette : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private AudioClip[] songs;
+
+    private AudioSource audioSource;
+    private int currentSongIndex = 0;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (songs.Length > 0)
+        {
+            audioSource.clip = songs[currentSongIndex];
+        }
+    }
+
+    public void PlayNextSong()
+    {
+        if (songs.Length == 0) return;
+        audioSource.clip = songs[currentSongIndex];
+        audioSource.Play();
+
+        currentSongIndex = (currentSongIndex + 1) % songs.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
